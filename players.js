59cca3ecb9ae8f7f606885410ee400b099a41878
antonellagon16 <526AgonzalezMaurera@frhsd.com>
2025-05-14 11:13:43 -0400

    pictures

--- players.js
+++ players.js
@@ -1,38 +1,38 @@
 const players = [
-    {
-        firstName: 'Aaron',
-        lastName: 'Judge',
-        position: 'RF',
-        age: 33,
-        photo: 'https://picsum.photos/200/300'
-    },
-    {
-        firstName: 'Aaron',
-        lastName: 'Judge',
-        position: 'RF',
-        age: 33,
-        photo: 'https://picsum.photos/200/300'
-    },
-    {
-        firstName: 'Aaron',
-        lastName: 'Judge',
-        position: 'RF',
-        age: 33,
-        photo: 'https://picsum.photos/200/300'
-    },
-    {
-        firstName: 'Aaron',
-        lastName: 'Judge',
-        position: 'RF',
-        age: 33,
-        photo: 'https://picsum.photos/200/300'
-    },
-    {
-        firstName: 'Aaron',
-        lastName: 'Judge',
-        position: 'RF',
-        age: 33,
-        photo: 'https://picsum.photos/200/300'
-    }
+    { firstName: 'Carlos', lastName: 'Rodón', position: 'SP', age: 32, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Max', lastName: 'Fried', position: 'SP', age: 31, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Marcus', lastName: 'Stroman', position: 'SP', age: 30, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Clarke', lastName: 'Schmidt', position: 'SP', age: 29, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Will', lastName: 'Warren', position: 'SP', age: 25, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Luke', lastName: 'Weaver', position: 'RP', age: 31, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Devin', lastName: 'Williams', position: 'RP', age: 30, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Ian', lastName: 'Hamilton', position: 'RP', age: 29, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Tim', lastName: 'Hill', position: 'RP', age: 35, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Mark', lastName: 'Leiter Jr.', position: 'RP', age: 34, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Tyler', lastName: 'Matzek', position: 'RP', age: 34, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Fernando', lastName: 'Cruz', position: 'RP', age: 35, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Yerry', lastName: 'De Los Santos', position: 'RP', age: 27, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Ryan', lastName: 'Yarbrough', position: 'RP', age: 33, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Jonathan', lastName: 'Loáisiga', position: 'RP', age: 30, photo: 'https://picsum.photos/200/300' },
+
+    { firstName: 'Austin', lastName: 'Wells', position: 'C', age: 25, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'J.C.', lastName: 'Escarra', position: 'C', age: 30, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Ben', lastName: 'Rice', position: 'C', age: 26, photo: 'https://picsum.photos/200/300' },
+
+    { firstName: 'Paul', lastName: 'Goldschmidt', position: '1B', age: 37, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'DJ', lastName: 'LeMahieu', position: '2B/3B', age: 36, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Jazz', lastName: 'Chisholm Jr.', position: '2B', age: 27, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Anthony', lastName: 'Volpe', position: 'SS', age: 24, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Oswald', lastName: 'Peraza', position: 'SS/3B', age: 24, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Pablo', lastName: 'Reyes', position: 'INF', age: 31, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Jorbit', lastName: 'Vivas', position: '2B', age: 24, photo: 'https://picsum.photos/200/300' },
+
+    { firstName: 'Aaron', lastName: 'Judge', position: 'RF', age: 33, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Cody', lastName: 'Bellinger', position: 'CF/1B', age: 29, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Jasson', lastName: 'Domínguez', position: 'CF', age: 22, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Oswaldo', lastName: 'Cabrera', position: 'LF/INF', age: 25, photo: 'https://picsum.photos/200/300' },
+    { firstName: 'Trent', lastName: 'Grisham', position: 'OF', age: 28, photo: 'https://picsum.photos/200/300' },
+
+    { firstName: 'Giancarlo', lastName: 'Stanton', position: 'DH', age: 35, photo: 'https://picsum.photos/200/300' }
 
 ]
