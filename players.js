bf8304023125aa00c19e6f012e13b204113f3f68
antonellagon16 <526AgonzalezMaurera@frhsd.com>
2025-05-14 10:49:10 -0400

    first commit

---
+++ players.js
@@ -0,0 +1,38 @@
+const players = [
+    {
+        firstName: 'Aaron',
+        lastName: 'Judge',
+        position: 'RF',
+        age: 33,
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Aaron',
+        lastName: 'Judge',
+        position: 'RF',
+        age: 33,
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Aaron',
+        lastName: 'Judge',
+        position: 'RF',
+        age: 33,
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Aaron',
+        lastName: 'Judge',
+        position: 'RF',
+        age: 33,
+        photo: 'https://picsum.photos/200/300'
+    },
+    {
+        firstName: 'Aaron',
+        lastName: 'Judge',
+        position: 'RF',
+        age: 33,
+        photo: 'https://picsum.photos/200/300'
+    }
+
+]
